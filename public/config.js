c165945c8662348df2997090f3702e9c0b803c3c
Jayesh Raktade <jayeshraktade7676@gmail.com>
2026-04-22 20:27:37 +0530

    initial commit

---
+++ public/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfSrekuiHVXbJTE3Sd3JD9hCJVJG8LbSg",
+  authDomain: "dinehub-6c443.firebaseapp.com",
+  projectId: "dinehub-6c443",
+  storageBucket: "dinehub-6c443.firebasestorage.app",
+  messagingSenderId: "454259305605",
+  appId: "1:454259305605:web:b30fdad5d6c56bd8450dec"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
